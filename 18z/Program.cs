684a86f9a99396326601746cc5f7f892eef90bce
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-07 16:37:45 +0500

    18z_last

--- 18z/Program.cs
+++ 18z/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.clear();
+﻿Console.Clear();
+Console.WriteLine("Введите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n > 4 || n < 1)
+{
+    Console.WriteLine("Введена неверная четверть, введите ещё раз: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1)
+Console.WriteLine("Возможные координаты: x > 0, y > 0");
+else if (n == 2)
+Console.WriteLine("Возможные координаты: x < 0, y > 0");
+else if (n == 3)
+Console.WriteLine("Возможные координаты: x < 0, y < 0");
+else
+Console.WriteLine("Возможные координаты: x > 0, y < 0");
